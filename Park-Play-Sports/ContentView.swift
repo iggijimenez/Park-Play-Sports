9a5d5b86cd3c59fdf054ab587296c3b1bad49c85
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2024-02-27 19:28:35 -0800

    Home page is complete

--- Park-Play-Sports/ContentView.swift
+++ Park-Play-Sports/ContentView.swift
@@ -6,21 +6,59 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.05), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            TabView {
+                HomePageView(region: $region)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HomePageView: View {
+    @Binding var region: MKCoordinateRegion
+    
+    var body: some View {
+        ZStack {
+            Color(.init(red: 0.2, green: 0.6, blue: 0.5, alpha: 0.9))
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Park Play Sports")
+                    .font(.title)
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                    .padding(2)
+                
+                Text("Discover, Play, You")
+                    .font(.caption)
+                    .fontWeight(.regular)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+                //                            .frame(width: 400, height: 300)
+                    .frame(width: 300, height: 400)
+                    .cornerRadius(10)
+                //                    .padding()
+                Spacer()
+                
+            }
+        }
+        .tabItem() {
+            Label("Order", systemImage: "square.and.pencil")
+        }
     }
 }
