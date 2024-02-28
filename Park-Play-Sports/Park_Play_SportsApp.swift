7f5ba4b867298329789e6a96410e8043f83b5959
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2024-02-27 18:29:10 -0800

    Initial Commit

---
+++ Park-Play-Sports/Park_Play_SportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Park_Play_SportsApp.swift
+//  Park-Play-Sports
+//
+//  Created by jimenez on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Park_Play_SportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
